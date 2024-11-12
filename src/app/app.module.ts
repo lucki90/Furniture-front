36e3f13ce4a55164e09efb0626613b73cda7d01e
lucki90 <tomlucki@wp.pl>
2024-11-12 23:40:59 +0100

    g

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AloneCabinetComponent } from './alone-cabinet/alone-cabinet.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    AloneCabinetComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    FormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        AloneCabinetComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        FormsModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
