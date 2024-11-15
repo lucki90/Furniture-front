850e05d758dd053bda8d9af2706a4fdc53cfdcc1
lucki90 <tomlucki@wp.pl>
2024-11-15 23:06:20 +0100

    first commit

--- src/app/alone-cabinet.service.ts
+++ src/app/alone-cabinet.service.ts
@@ -2,28 +2,16 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-interface CabinetRequest {
-  x: number;
-  y: number;
-  z: number;
-}
-
-interface CabinetResponse {
-  x: number;
-  y: number;
-  z: number;
-}
-
 @Injectable({
   providedIn: 'root'
 })
 export class AloneCabinetService {
 
-  private apiUrl = 'http://localhost:8080/alone/calculate';
+  private apiUrl = 'http://localhost:8080/api/furniture/free-standing/calculate';
 
   constructor(private http: HttpClient) { }
 
-  calculate(data: CabinetRequest): Observable<CabinetResponse> {
-    return this.http.post<CabinetResponse>(this.apiUrl, data);
+  calculateCabinet(data: any): Observable<any> {
+    return this.http.post<any>(this.apiUrl, data);
   }
 }
--- src/app/alone-cabinet/alone-cabinet.component.ts
+++ src/app/alone-cabinet/alone-cabinet.component.ts
@@ -7,28 +7,32 @@ import { AloneCabinetService } from '../alone-cabinet.service';
   styleUrls: ['./alone-cabinet.component.css']
 })
 export class AloneCabinetComponent {
-  x: number = 0;
-  y: number = 0;
-  z: number = 0;
-  result: any = null;
+  height: number = 0;
+  width: number = 0;
+  depth: number = 0;
+  shelfQuantity: number = 1;
+  oneFront: boolean = true;
+  response: any;
 
 
-  constructor(private aloneCabinetService: AloneCabinetService) { }
+  constructor(private cabinetService: AloneCabinetService) { }
 
-  onSubmit() {
-    const requestData = { x: this.x, y: this.y, z: this.z };
-    this.aloneCabinetService.calculate(requestData).subscribe(
+  calculateCabinet() {
+    const requestBody = {
+      height: this.height,
+      width: this.width,
+      depth: this.depth,
+      shelfQuantity: this.shelfQuantity,
+      oneFront: this.oneFront
+    };
+
+    this.cabinetService.calculateCabinet(requestBody).subscribe(
       (response) => {
-        this.result = response;
+        this.response = response;
       },
       (error) => {
         console.error('Error:', error);
       }
     );
-  } 
-
-
-  // ngOnInit(): void {
-  // }
-
+  }
 }
